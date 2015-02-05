f26a26238082efb9af6b979a78ba5e18ea304d98
Chris Perry <byndcivilization@gmail.com>
2015-02-05 15:19:49 -0500

    added compose.io connection code

--- loader.py
+++ loader.py
@@ -13,7 +13,9 @@ from pymongo import MongoClient
 def mongo_load(data,db_name,collection_name,username,password):
 
 	# Make db connection
+	# MONGO_URL = 'mongodb://' + username + ':' + password + '@c726.candidate.19.mongolayer.com:10726,c582.candidate.32.mongolayer.com:10582/rgi2015_dev'
 	client = MongoClient('localhost',27017)
+	client = MongoClient(MONGO_URL)
 	db = client[db_name]
 	collection = db[collection_name]
 
--- parser.py
+++ parser.py
@@ -41,229 +41,229 @@ def parse(sheet_name, sheet, data):
 			# data[-1][rkey[col].replace(" ", "_")] = a
 			data[-1]['question_order'] = int(a)
 
-		# row_id
-		col = lkey['row_id']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1]['old_reference'][rkey[col].replace(" ", "_")] = a
+			# row_id
+			col = lkey['row_id']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1]['old_reference'][rkey[col].replace(" ", "_")] = a
 
-		# row_id_org
-		col = lkey['row_id_org']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1]['old_reference'][rkey[col].replace(" ", "_")] = a
+			# row_id_org
+			col = lkey['row_id_org']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1]['old_reference'][rkey[col].replace(" ", "_")] = a
 
-		# old_rwi_questionnaire_code
-		col = lkey['old_rwi_questionnaire_code']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1]['old_reference'][rkey[col].replace(" ", "_")] = a
+			# old_rwi_questionnaire_code
+			col = lkey['old_rwi_questionnaire_code']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1]['old_reference'][rkey[col].replace(" ", "_")] = a
 
-		# uid
-		col = lkey['uid']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1]['old_reference'][rkey[col].replace(" ", "_")] = a
-			
-		# qid
-		col = lkey['qid']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1]['old_reference'][rkey[col].replace(" ", "_")] = a
-			
-		# indaba_question_order
-		col = lkey['indaba_question_order']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1]['old_reference'][rkey[col].replace(" ", "_")] = a
+			# uid
+			col = lkey['uid']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1]['old_reference'][rkey[col].replace(" ", "_")] = a
+				
+			# qid
+			col = lkey['qid']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1]['old_reference'][rkey[col].replace(" ", "_")] = a
+				
+			# indaba_question_order
+			col = lkey['indaba_question_order']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1]['old_reference'][rkey[col].replace(" ", "_")] = a
 
-		# component
-		col = lkey['component']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1]['old_reference']['component_excel'] = a
+			# component
+			col = lkey['component']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1]['old_reference']['component_excel'] = a
 
-		data[-1]['component'] = sheet_id
-		data[-1]['component_text'] = sheet_text
+			data[-1]['component'] = sheet_id
+			data[-1]['component_text'] = sheet_text
 
 
-		# indicator_name
-		col = lkey['indicator_name']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1][rkey[col].replace(" ", "_")] = a
+			# indicator_name
+			col = lkey['indicator_name']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1][rkey[col].replace(" ", "_")] = a
 
-		# sub_indicator_name
-		col = lkey['sub_indicator_name']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1][rkey[col].replace(" ", "_")] = a
+			# sub_indicator_name
+			col = lkey['sub_indicator_name']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1][rkey[col].replace(" ", "_")] = a
 
-		# minstry_if_applicable
-		col = lkey['minstry_if_applicable']
-		a = get_cell(sheet,row,col)
-		if not a:
-			data[-1]['ministry'] = 'none'
-		else:
-			data[-1]['ministry'] = a
+			# minstry_if_applicable
+			col = lkey['minstry_if_applicable']
+			a = get_cell(sheet,row,col)
+			if not a:
+				data[-1]['ministry'] = 'none'
+			else:
+				data[-1]['ministry'] = a
 
-		# section_name
-		col = lkey['section_name']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1][rkey[col].replace(" ", "_")] = a
+			# section_name
+			col = lkey['section_name']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1][rkey[col].replace(" ", "_")] = a
 
-		# parent_question
-		col = lkey['parent_question']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1]['question_text'] = a
+			# parent_question
+			col = lkey['parent_question']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1]['question_text'] = a
+				
+			# child_question
+			col = lkey['child_question']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1][rkey[col].replace(" ", "_")] = a
 			
-		# child_question
-		col = lkey['child_question']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1][rkey[col].replace(" ", "_")] = a
-		
-		# choice_1
-		col = lkey['choice_1']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1]['question_choices'].append({'name' : rkey[col], 'order' : int(rkey[col].replace("choice_", "")), 'criteria' : a})
-			data[-1]['options'] = 1
-			
-		# choice_2
-		col = lkey['choice_2']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1]['question_choices'].append({'name' : rkey[col], 'order' : int(rkey[col].replace("choice_", "")), 'criteria' : a})
-			data[-1]['options'] += 1
-			
-		# choice_3
-		col = lkey['choice_3']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1]['question_choices'].append({'name' : rkey[col], 'order' : int(rkey[col].replace("choice_", "")), 'criteria' : a})
-			data[-1]['options'] += 1
-			
-		# choice_4
-		col = lkey['choice_4']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1]['question_choices'].append({'name' : rkey[col], 'order' : int(rkey[col].replace("choice_", "")), 'criteria' : a})
-			data[-1]['options'] += 1
-			
-		# choice_5
-		col = lkey['choice_5']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1]['question_choices'].append({'name' : rkey[col], 'order' : int(rkey[col].replace("choice_", "")), 'criteria' : a})
-			data[-1]['options'] += 1
+			# choice_1
+			col = lkey['choice_1']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1]['question_choices'].append({'name' : rkey[col], 'order' : int(rkey[col].replace("choice_", "")), 'criteria' : a})
+				data[-1]['options'] = 1
+				
+			# choice_2
+			col = lkey['choice_2']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1]['question_choices'].append({'name' : rkey[col], 'order' : int(rkey[col].replace("choice_", "")), 'criteria' : a})
+				data[-1]['options'] += 1
+				
+			# choice_3
+			col = lkey['choice_3']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1]['question_choices'].append({'name' : rkey[col], 'order' : int(rkey[col].replace("choice_", "")), 'criteria' : a})
+				data[-1]['options'] += 1
+				
+			# choice_4
+			col = lkey['choice_4']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1]['question_choices'].append({'name' : rkey[col], 'order' : int(rkey[col].replace("choice_", "")), 'criteria' : a})
+				data[-1]['options'] += 1
+				
+			# choice_5
+			col = lkey['choice_5']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1]['question_choices'].append({'name' : rkey[col], 'order' : int(rkey[col].replace("choice_", "")), 'criteria' : a})
+				data[-1]['options'] += 1
 
-		# NRC Precept
-		col = lkey['nrc_precept']
-		a = get_cell(sheet,row,col)
-		if not a:
-			pass
-		else:
-			data[-1][rkey[col].replace(" ", "_")] = int(a)
+			# NRC Precept
+			col = lkey['nrc_precept']
+			a = get_cell(sheet,row,col)
+			if not a:
+				pass
+			else:
+				data[-1][rkey[col].replace(" ", "_")] = int(a)
 
-		data[-1]['question_choices'].append({'modifiedBy' : 'initiated', 'modifiedDate' : datetime.now().isoformat()})
-			
-		# # Reason for Inclusion 
-		# col = lkey['']
-		# a = get_cell(sheet,row,col)
-		# if not a:
-		# 	pass
-		# else:
-		# 	data[-1][rkey[col].replace(" ", "_")] = a
-			
-		# # EITI
-		# col = lkey['eiti']
-		# a = get_cell(sheet,row,col)
-		# if not a:
-		# 	pass
-		# else:
-		# 	data[-1][rkey[col].replace(" ", "_")] = a
-			
-		# # Comments
-		# col = lkey['comments']
-		# a = get_cell(sheet,row,col)
-		# if not a:
-		# 	pass
-		# else:
-		# 	data[-1][rkey[col].replace(" ", "_")] = a
-			
-		# # Proposed changes
-		# col = lkey['proposed_changes']
-		# a = get_cell(sheet,row,col)
-		# if not a:
-		# 	pass
-		# else:
-		# 	data[-1][rkey[col].replace(" ", "_")] = a
-			
-		# # 1= New, 2= Changed, 3=Answer needs fixing, 4= Needs revision, 5=delete
-		# col = lkey['1=_new,_2=_changed,_3=answer_needs_fixing,_4=_needs_revision,_5=delete']
-		# a = get_cell(sheet,row,col)
-		# if not a:
-		# 	data[-1]['0=unchanged,_1=_new,_2=_changed,_3=answer_needs_fixing,_4=_needs_revision,_5=delete'] = '0'
-		# else:
-		# 	data[-1]['0=unchanged,_1=_new,_2=_changed,_3=answer_needs_fixing,_4=_needs_revision,_5=delete'] = a
-			
-		# # Scoring (e.g. ordinal, cardinal, binary, other)
-		# col = lkey['scoring_(e.g._ordinal,_cardinal,_binary,_other)']
-		# a = get_cell(sheet,row,col)
-		# if not a:
-		# 	pass
-		# else:
-		# 	data[-1][rkey[col].replace(" ", "_")] = a
-			
-		# # De facto/De jure
-		# col = lkey['de_facto/de_jure']
-		# a = get_cell(sheet,row,col)
-		# if not a:
-		# 	pass
-		# else:
-		# 	data[-1][rkey[col].replace(" ", "_")] = a
-			
-		# # Government effectiveness (excluding disclosure)
-		# col = lkey['government_effectiveness_(excluding_disclosure)']
-		# a = get_cell(sheet,row,col)
-		# if not a:
-		# 	pass
-		# else:
-		# 	data[-1][rkey[col].replace(" ", "_")] = a
+			data[-1]['modified'].append({'modifiedBy' : 'initiated', 'modifiedDate' : '' + datetime.now().isoformat()})
+				
+			# # Reason for Inclusion 
+			# col = lkey['']
+			# a = get_cell(sheet,row,col)
+			# if not a:
+			# 	pass
+			# else:
+			# 	data[-1][rkey[col].replace(" ", "_")] = a
+				
+			# # EITI
+			# col = lkey['eiti']
+			# a = get_cell(sheet,row,col)
+			# if not a:
+			# 	pass
+			# else:
+			# 	data[-1][rkey[col].replace(" ", "_")] = a
+				
+			# # Comments
+			# col = lkey['comments']
+			# a = get_cell(sheet,row,col)
+			# if not a:
+			# 	pass
+			# else:
+			# 	data[-1][rkey[col].replace(" ", "_")] = a
+				
+			# # Proposed changes
+			# col = lkey['proposed_changes']
+			# a = get_cell(sheet,row,col)
+			# if not a:
+			# 	pass
+			# else:
+			# 	data[-1][rkey[col].replace(" ", "_")] = a
+				
+			# # 1= New, 2= Changed, 3=Answer needs fixing, 4= Needs revision, 5=delete
+			# col = lkey['1=_new,_2=_changed,_3=answer_needs_fixing,_4=_needs_revision,_5=delete']
+			# a = get_cell(sheet,row,col)
+			# if not a:
+			# 	data[-1]['0=unchanged,_1=_new,_2=_changed,_3=answer_needs_fixing,_4=_needs_revision,_5=delete'] = '0'
+			# else:
+			# 	data[-1]['0=unchanged,_1=_new,_2=_changed,_3=answer_needs_fixing,_4=_needs_revision,_5=delete'] = a
+				
+			# # Scoring (e.g. ordinal, cardinal, binary, other)
+			# col = lkey['scoring_(e.g._ordinal,_cardinal,_binary,_other)']
+			# a = get_cell(sheet,row,col)
+			# if not a:
+			# 	pass
+			# else:
+			# 	data[-1][rkey[col].replace(" ", "_")] = a
+				
+			# # De facto/De jure
+			# col = lkey['de_facto/de_jure']
+			# a = get_cell(sheet,row,col)
+			# if not a:
+			# 	pass
+			# else:
+			# 	data[-1][rkey[col].replace(" ", "_")] = a
+				
+			# # Government effectiveness (excluding disclosure)
+			# col = lkey['government_effectiveness_(excluding_disclosure)']
+			# a = get_cell(sheet,row,col)
+			# if not a:
+			# 	pass
+			# else:
+			# 	data[-1][rkey[col].replace(" ", "_")] = a
